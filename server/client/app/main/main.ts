21170311a4d485cb9d6ee73604cb76be52bf2be3
jacekburys <jacekburys@gmail.com>
2016-10-28 23:29:09 +0100

    server init

---
+++ server/client/app/main/main.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('serverApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });
