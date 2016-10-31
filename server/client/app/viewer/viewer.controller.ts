52dceaf353a4158ea8476c0b170c13ce09280719
jacekburys <jacekburys@gmail.com>
2016-10-31 18:38:56 +0000

    simple UI

--- server/client/app/viewer/viewer.controller.ts
+++ server/client/app/viewer/viewer.controller.ts
@@ -2,17 +2,104 @@
 
 (function() {
 
+enum CalibrationStatus {
+  Calibrated = <any>'Calibrated',
+  InProgress = <any>'InProgress',
+  Error = <any>'Error',
+}
+
 class ViewerController {
   constructor($scope) {
     this.connectedClients = [
       {
         id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
       },
       {
         id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
+      },
+      {
+        id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
+      },
+      {
+        id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
+      },
+      {
+        id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
+      },
+      {
+        id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
+      },
+      {
+        id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
+      },
+      {
+        id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
+      },
+      {
+        id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
+      },
+      {
+        id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
+      },
+      {
+        id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
+      },
+      {
+        id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
+      },
+      {
+        id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
+      },
+      {
+        id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
+      },
+      {
+        id : 123,
+        ip : '127.0.0.1',
+        calibStatus : CalibrationStatus.Calibrated,
+      },
+      {
+        id : 456,
+        ip : '148.0.1.43',
+        calibStatus : CalibrationStatus.Error,
       },
     ];
+  }
 
+  takePicture() {
+    console.log('Trying to take a picture');
+  }
+
+  calibrate() {
+    console.log('Trying to calibrate');
   }
 }
 
--- server/server/routes.js
+++ server/server/routes.js
@@ -9,6 +9,8 @@ import path from 'path';
 
 export default function(app) {
   // Insert routes below
+  app.use('/api/pointify_client', require('./api/pointify_client'));
+  // TODO : delete Things later
   app.use('/api/things', require('./api/thing'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
