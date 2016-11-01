9b94b2d8e5b14e8b3dce48e34d58e4bc153c0e10
= <hosieradam@gmail.com>
2016-11-01 18:57:56 +0000

    more calibration implementation w/ ayman

--- client/src/calibration.hpp
+++ client/src/calibration.hpp
@@ -4,6 +4,15 @@
 class Calibration 
 {
 private:
+    // amount of markers in the x and y directions on the grid
+    const int MARKER_X = 1;
+    const int MARKER_Y = 1;
+    
+    // size of marker in meters(?)
+    const int MARKER_LENGTH = 1;
+    
+    // distance between markers
+    const int MARKER_SEPARATION = 1;
 public:
     Calibration();
     void calibrate();
