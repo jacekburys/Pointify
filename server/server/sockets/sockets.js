b0c5ff047ce6c0cd15c40ac286d87d6560d84450
jacekburys <jacekburys@gmail.com>
2016-11-02 17:12:40 +0000

    way of receiving a frame

--- server/server/sockets/sockets.js
+++ server/server/sockets/sockets.js
@@ -3,6 +3,10 @@ export default function(socketio) {
   socketio.on('connection', function(socket) {
     console.log('a user connected');
     socket.emit('hello');
+    socket.on('new_frame', function(frame) {
+      console.log('new frame');
+      console.log(frame);
+    });
     socket.on('disconnect', function() {
       console.log('disconnected');
     });
