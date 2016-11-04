8fbe908d8ad91fd67e4b7504745b833a331f3954
= <hosieradam@gmail.com>
2016-11-04 10:07:48 +0000

    added includes to calibration classes, and fixed typos

--- client/src/calibration.cpp
+++ client/src/calibration.cpp
@@ -1,30 +1,29 @@
 #include "calibration.hpp"
-//TODO: opencv includes
+#include <opencv2/aruco.hpp>
+#include <vector>
 
 Calibration::Calibration()
 {
 
 }
 
-void Calibration::calibrate()
+void Calibration::calibrate(cv::Mat img)
 {
-    cv::Mat img = /*TODO: get input image somehow (parameter/sensor?)*/
-
     // dict is the set of aruco markers that we are expecting to see
     cv::Ptr<cv::aruco::Dictionary> dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
 
     // perform marker detection
-    vector<vector<Point2f>> corners;
-    vector<int> ids;
+    std::vector<std::vector<cv::Point2f>> corners;
+    std::vector<int> ids;
 
     cv::aruco::detectMarkers(img, dict, corners, ids);
 
     // perform camera calibration
-    cv::Ptr<cv::aruco::Board> board = aruco::GridBoard::create(MARKER_X, MARKER_Y, MARKER_LENGTH, MARKER_SEPARATION, dict).staticCast<aruco::Board>();
-    cv::Mat cameraMatrix = cv::Mat::eye(3, 3, cv::CV_64F)
-    cv::Mat distCoeffs = /* TODO: getIrCameraParams() -> to Mat */
-    cv::Mat rvects, tvecs;
+    cv::Ptr<cv::aruco::Board> board = cv::aruco::GridBoard::create(MARKER_X, MARKER_Y, MARKER_LENGTH, MARKER_SEPARATION, dict).staticCast<cv::aruco::Board>();
+    cv::Mat cameraMatrix = cv::Mat::eye(3, 3, CV_64F);
+    cv::Mat distCoeffs = NULL;/* TODO: getIrCameraParams() -> to Mat */
+    cv::Mat rvecs, tvecs; // rotation and translation vectors that bring the marker from it's coordinate space to world coordinate space  
  
-    cv::aruco::calibrateCameraAruco(corners, ids, MARKER_X * MARKER_Y, board, img.size(), cameraMatrix, distCoeffs, rvecs, tvecs)
+    cv::aruco::calibrateCameraAruco(corners, ids, MARKER_X * MARKER_Y, board, img.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
 }
 
