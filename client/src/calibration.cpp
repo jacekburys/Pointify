154d92baaa65500f97abafe8ed52500008b4c74f
Ayman <aymanm95@gmail.com>
2016-11-07 13:16:03 +0000

    Changed calibration.cpp/hpp for OOP style, working on distCoeffs now

--- client/src/calibration.cpp
+++ client/src/calibration.cpp
@@ -4,26 +4,32 @@
 
 Calibration::Calibration()
 {
-
+	// dict is the set of aruco markers that we are expecting to see
+    dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
 }
 
-void Calibration::calibrate(cv::Mat img)
+/*Calibration::~Calibration()
 {
-    // dict is the set of aruco markers that we are expecting to see
-    cv::Ptr<cv::aruco::Dictionary> dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+	
+}*/
 
-    // perform marker detection
-    std::vector<std::vector<cv::Point2f>> corners;
-    std::vector<int> ids;
-
+bool Calibration::calibrate(cv::Mat img)
+{
+    
+	//Perform marker detection
     cv::aruco::detectMarkers(img, dict, corners, ids);
 
     // perform camera calibration
     cv::Ptr<cv::aruco::Board> board = cv::aruco::GridBoard::create(MARKER_X, MARKER_Y, MARKER_LENGTH, MARKER_SEPARATION, dict).staticCast<cv::aruco::Board>();
+	
+	// cv::Mat is (height,width) 3x3 camera matrix
     cv::Mat cameraMatrix = cv::Mat::eye(3, 3, CV_64F);
+	
     cv::Mat distCoeffs = NULL;/* TODO: getIrCameraParams() -> to Mat */
     cv::Mat rvecs, tvecs; // rotation and translation vectors that bring the marker from it's coordinate space to world coordinate space  
  
     cv::aruco::calibrateCameraAruco(corners, ids, MARKER_X * MARKER_Y, board, img.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
+	
+	return true;
 }
 
