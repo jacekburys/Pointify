39bb446cc2d7989dec3537a6eadbc7c5502e9ba9
Ayman <aymanm95@gmail.com>
2016-11-10 14:29:27 +0000

    Compilation fixes, still not compiling

--- client/src/calibration.cpp
+++ client/src/calibration.cpp
@@ -1,6 +1,7 @@
 #include "calibration.hpp"
+#include "libfreenect2/libfreenect2.hpp"
 #include <opencv2/aruco.hpp>
-#include <vector>
+#include <vector
 
 Calibration::Calibration(libfreenect2::Freenect2Device *device)
 {
@@ -24,7 +25,9 @@ bool Calibration::calibrate(cv::Mat img)
 
     // perform camera calibration
     cv::Ptr<cv::aruco::Board> board = cv::aruco::GridBoard::create(MARKER_X, MARKER_Y, MARKER_LENGTH, MARKER_SEPARATION, dict).staticCast<cv::aruco::Board>();
-       
+   
+   // Unsure about this bit 
+
     //Get the depth parameters from device 
     Freenect2Device::IrCameraParams depthParameters = device->getIrCameraParams();
     
@@ -41,11 +44,17 @@ bool Calibration::calibrate(cv::Mat img)
      {0,  0, 1}
     };    
 
+    //
+
     // (width,height)
     cv::Mat cameraMatrix = Mat(3,3,CV_64F,depthMatrix);
-    cv::Mat distCoeffs;
-    cv::Mat rvecs, tvecs; // rotation and translation vectors that bring the marker from it's coordinate space to world coordinate space  
- 
+    cv::Mat distCoeffs = Mat::zeros(5, 1, CV_64F);    
+   
+    // rotation (rvecs) and translation (tvecs) vectors that bring the marker from it's coordinate space to world coordinate space
+    //std::vector<Mat> rvecs, tvecs;
+    cd::Mat rvecs, tvecs;
+
+    // calibrate
     cv::aruco::calibrateCameraAruco(corners, ids, MARKER_X * MARKER_Y, board, img.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
 	
     return true;
